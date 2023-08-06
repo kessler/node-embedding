cf5efd2054196c0e0ebf74bfe880e7c2b1af613a
Yaniv Kessler <yanivk@gmail.com>
2023-08-06 07:14:50 +0300

    feat: SimpleStorage ready and tested

---
+++ index.mjs
@@ -0,0 +1,11 @@
+//import loadProviders from './lib/providers.mjs'
+import OpenAIEmbedder from './lib/OpenAIEmbedder.mjs'
+import SimpleStorage from './lib/SimpleStorage.mjs'
+import Collection from './lib/Collection'
+
+export { OpenAIEmbedder, SimpleStorage, Collection }
+
+// export async function create(embedderName, storage) {
+  
+// }
+
--- test/SimpleStorage.test.mjs
+++ test/SimpleStorage.test.mjs
@@ -3,7 +3,7 @@ import SimpleStorage from '../lib/SimpleStorage.mjs'
 
 test('add() entries to storage', async t => {
   const mockFs = new MockFS()
-  const storage = new SimpleStorage('test/data.json', mockFs)
+  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
   await storage.add('test', [1, 2, 3], { foo: 1 })
   t.is(mockFs.filename, 'test/data.json')
   t.is(mockFs.data, '[[[1,2,3],{"text":"test","embedding":[1,2,3],"metadata":{"foo":1}}]]')
@@ -11,7 +11,7 @@ test('add() entries to storage', async t => {
 
 test('delete() entries from storage', async t => {
   const mockFs = new MockFS()
-  const storage = new SimpleStorage('test/data.json', mockFs)
+  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
   const { id } = await storage.add('test', [1, 2, 3], { foo: 1 })
   await storage.delete(id)
   t.is(mockFs.data, '[]')
@@ -19,20 +19,23 @@ test('delete() entries from storage', async t => {
 
 test('get() entries from storage', async t => {
   const mockFs = new MockFS()
-  const storage = new SimpleStorage('test/data.json', mockFs)
+  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
   const { id } = await storage.add('test', [1, 2, 3], { foo: 1 })
   const entry = await storage.get(id)
   t.deepEqual(entry, { text: 'test', embedding: [1, 2, 3], metadata: { foo: 1 } })
 })
 
-test.only('query() entries from storage', async t => {
+test('query() entries from storage', async t => {
   const mockFs = new MockFS('[[[1,2,3],{"text":"test","embedding":[1,2,3],"metadata":{"foo":1}}]]')
-  const storage = new SimpleStorage('test/data.json', mockFs)
+  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
   const results = await storage.query([1, 2, 3], { maxResults: 10, threshold: 0.8 })
   
   t.deepEqual(results, [{ similarity: 1, text: 'test', metadata: { foo: 1 } }])
 })
 
+test.skip('query() entries from storage with maxResults', async t => {
+
+})
 
 class MockFS {
   constructor(data = '[]') {
