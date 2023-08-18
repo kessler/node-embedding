0e476ad7e17e8dddd99a43eef61058da548a0d44
Yaniv Kessler <yanivk@gmail.com>
2023-08-19 02:47:27 +0300

    refactor: SimpleStorage is now FileSystemStorage. FileSystemStorage is one underlying storage for many collections, instead of storage to collection mapping, which was a wrong design (although this made it more complex of course

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This module is built to allow progressive advancement from simple json based embedding database to more advanced solutions like chroma or redis.
 
+## resources
+- https://supabase.com/blog/openai-embeddings-postgres-vector
--- test/SimpleStorage.test.mjs
+++ test/SimpleStorage.test.mjs
@@ -2,33 +2,34 @@ import test from 'ava'
 import SimpleStorage from '../lib/SimpleStorage.mjs'
 
 test('add() entries to storage', async t => {
-  const mockFs = new MockFS()
-  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
-  await storage.add('test', [1, 2, 3], { foo: 1 })
-  t.is(mockFs.filename, 'test/data.json')
+  const { mockFs, storage } = t.context
+  
+  await storage.add('data', 'test', [1, 2, 3], { foo: 1 })
+  t.is(mockFs.writeFilename, 'test/data.json')
+  t.is(mockFs.readFilename, 'test/data.json')
   t.is(mockFs.data, '[[[1,2,3],{"text":"test","embedding":[1,2,3],"metadata":{"foo":1}}]]')
 })
 
 test('delete() entries from storage', async t => {
-  const mockFs = new MockFS()
-  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
-  const { id } = await storage.add('test', [1, 2, 3], { foo: 1 })
-  await storage.delete(id)
+  const { mockFs, storage } = t.context
+  
+  const { id } = await storage.add('data', 'test', [1, 2, 3], { foo: 1 })
+  await storage.delete('data', id)
   t.is(mockFs.data, '[]')
 })
 
 test('get() entries from storage', async t => {
-  const mockFs = new MockFS()
-  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
-  const { id } = await storage.add('test', [1, 2, 3], { foo: 1 })
-  const entry = await storage.get(id)
+  const { mockFs, storage } = t.context
+  
+  const { id } = await storage.add('data', 'test', [1, 2, 3], { foo: 1 })
+  const entry = await storage.get('data', id)
   t.deepEqual(entry, { text: 'test', embedding: [1, 2, 3], metadata: { foo: 1 } })
 })
 
 test('query() entries from storage', async t => {
-  const mockFs = new MockFS('[[[1,2,3],{"text":"test","embedding":[1,2,3],"metadata":{"foo":1}}]]')
-  const storage = new SimpleStorage({ filename: 'test/data.json', _fs: mockFs })
-  const results = await storage.query([1, 2, 3], { maxResults: 10, threshold: 0.8 })
+  const { mockFs, storage } = t.context
+  mockFs.data = '[[[1,2,3],{"text":"test","embedding":[1,2,3],"metadata":{"foo":1}}]]'
+  const results = await storage.query('data', [1, 2, 3], { maxResults: 10, threshold: 0.8 })
   
   t.deepEqual(results, [{ similarity: 1, text: 'test', metadata: { foo: 1 } }])
 })
@@ -37,17 +38,26 @@ test.skip('query() entries from storage with maxResults', async t => {
 
 })
 
+test.beforeEach(t => {
+  const mockFs = new MockFS()
+  const storage = new SimpleStorage(mockFs, { directory: 'test' })
+  t.context = {
+    mockFs, storage
+  }
+})
+
 class MockFS {
   constructor(data = '[]') {
     this.data = data
   }
 
-  async readFile() {
+  async readFile(filename) {
+    this.readFilename = filename
     return this.data
   }
 
   async writeFile(filename, data) {
-    this.filename = filename
+    this.writeFilename = filename
     this.data = data
   }
 }
